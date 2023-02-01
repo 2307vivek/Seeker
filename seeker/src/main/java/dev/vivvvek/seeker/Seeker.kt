888bbbe88bb919218ae31fbd750d739d1011eace
Vivek Singh <2307vivek@gmail.com>
2023-02-01 23:44:06 +0530

    created track for unsegmented seeker.

--- seeker/src/main/java/dev/vivvvek/seeker/Seeker.kt
+++ seeker/src/main/java/dev/vivvvek/seeker/Seeker.kt
@@ -15,22 +15,78 @@
  */
 package dev.vivvvek.seeker
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Seeker(
     value: Float,
-    range: ClosedFloatingPointRange<Float>,
+    range: ClosedFloatingPointRange<Float> = 0f..1f,
     onValueChange: (Float) -> Unit,
     onValueChangeFinished: (() -> Unit)? = null,
     segments: List<Segment> = emptyList(),
     enabled: Boolean = true,
     colors: SeekerColors = SeekerDefaults.seekerColors(),
+    dimensions: SeekerDimensions = SeekerDefaults.seekerDimensions(),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     modifier: Modifier = Modifier
 ) {
+    segments.forEach {
+        require(it.start in range) {
+            "segment must start from withing the range."
+        }
+    }
 
+    Track(
+        modifier = Modifier.fillMaxSize(),
+        enabled = enabled,
+        segments = segments,
+        colors = colors,
+        dimensions = dimensions
+    )
+}
+
+@Composable
+fun Track(
+    modifier: Modifier,
+    enabled: Boolean,
+    segments: List<Segment>,
+    colors: SeekerColors,
+    dimensions: SeekerDimensions
+) {
+    val trackColor = colors.trackColor(enabled)
+    val thumbRadius by dimensions.thumbRadius()
+    val trackHeight by dimensions.trackHeight()
+    var startPx: Float
+    var endPx: Float
+    Canvas(modifier = modifier) {
+        startPx = 0f + thumbRadius.toPx()
+        endPx = size.width - thumbRadius.toPx()
+        if (segments.isEmpty()) {
+            drawLine(
+                start = Offset(startPx, center.y),
+                end = Offset(endPx, center.y),
+                color = trackColor.value,
+                strokeWidth = trackHeight.toPx()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SeekerPreview() {
+    Seeker(
+        value = 0f,
+        onValueChange = {  },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
