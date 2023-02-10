6c9d650d13f64af2de80868064ae96ce76065137
Vivek Singh <2307vivek@gmail.com>
2023-02-10 17:54:10 +0530

    adding back MainActivity.kt

--- app/src/main/java/dev/vivvvek/seekerdemo/MainActivity.kt
+++ app/src/main/java/dev/vivvvek/seekerdemo/MainActivity.kt
@@ -18,15 +18,8 @@ package dev.vivvvek.seekerdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import dev.vivvvek.seeker.Seeker
 import dev.vivvvek.seekerdemo.ui.theme.SeekerTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +27,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SeekerTheme {
-
             }
         }
     }
