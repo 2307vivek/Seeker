f70695ae420ce4955b3b74aa12f16893b11d6b60
Vivek Singh <2307vivek@gmail.com>
2023-02-14 16:54:59 +0530

    created seeker-demo

--- app/src/main/java/dev/vivvvek/seekerdemo/NowPlayingViewModel.kt
+++ app/src/main/java/dev/vivvvek/seekerdemo/NowPlayingViewModel.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2023 Vivek Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.vivvvek.seekerdemo
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.vivvvek.seeker.Segment
@@ -8,11 +22,9 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
-class NowPlayingViewModel: ViewModel() {
+class NowPlayingViewModel : ViewModel() {
 
     private val _isPlaying = MutableStateFlow(false)
     val isPlaying: StateFlow<Boolean> = _isPlaying
@@ -39,8 +51,7 @@ class NowPlayingViewModel: ViewModel() {
 
         if (!_isPlaying.value) {
             job?.cancel()
-        }
-        else startPlaying()
+        } else startPlaying()
     }
 
     fun setPosition(position: Float) {
@@ -49,13 +60,14 @@ class NowPlayingViewModel: ViewModel() {
 
     private fun startPlaying() {
         job = viewModelScope.launch {
-            while (_isPlaying.value && _position.value <= length) {
+            while (_isPlaying.value && _position.value < length) {
                 delay(1000)
-
-                if (_position.value >= length)
-                    _isPlaying.value = false
-
                 _position.value += 1f
+
+                if (_position.value == length) {
+                    _isPlaying.value = false
+                    break
+                }
             }
         }
     }
